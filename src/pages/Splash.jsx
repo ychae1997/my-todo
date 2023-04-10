4e2e5bf7c526a5db2da6313df2a0e4d84c860131
ychae1997 <ychae1997@gmail.com>
2023-04-10 20:43:50 +0900

    feat: loading, splash

---
+++ src/pages/Splash.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Splash() {
+  return (
+    <div className="welcome">
+      <div className="splash"></div>
+      <div className="dot"></div>
+    </div>
+  );
+}
